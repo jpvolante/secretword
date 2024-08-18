59e8d65e37631da3fbb3ab3d8cc940f73d49de33
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-08-17 21:39:08 -0300

    Create database and fix react files

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordslist = { 
+	carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"], 
+	fruta: ["Banana", "Maçã", "Pēra", "Mamão", "Laranja"], 
+	corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"], 
+	computador: ["Mouse", "Teclado", "Monitor", "Gabinete"], 
+	programacão: ["Linguagem", "Framework", "Javascript", "React"], 
+	alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    disciplinas: ["História", "Computação", "Matemática", "Sociologia"],
+};
