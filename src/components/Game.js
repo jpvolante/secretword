c748b19e3156e814a52cec86277a0c4eab758484
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-09-14 22:18:41 -0300

    create event to insert letters

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState, useRef} from 'react';
 import './Game.css'
 
 const Game = ({
@@ -11,6 +12,19 @@ const Game = ({
   guesses,
   score, 
 }) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus();
+  };
+
   return (
     <div className='game'>
       <p className="points">
@@ -32,8 +46,15 @@ const Game = ({
       </div>
       <div className='letterContainer'>
         <p>Tente advinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" name='letter' maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+          name='letter' 
+          maxLength="1" 
+          required 
+          onChange={(e) => setLetter(e.target.value)} 
+          value={letter}
+          ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
