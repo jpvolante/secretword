1faa555cb97ee8c048d2530c639c831667e3142e
Joao Pedro Volante <joaopedrovolante@gmail.com>
2024-09-15 10:07:20 -0300

    create interaction with game

--- src/App.js
+++ src/App.js
@@ -71,8 +71,30 @@ function App() {
 
   // process the letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
+    
+    const normalizedLetter = letter.toLowerCase()
+
+    //check if letter has already been utilized
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+      return;
+    }
+
+    //push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ])
+    }else{
+      setGuessedLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+    }; 
+
   }
+  console.log(guessedLetters);
+  console.log(wrongLetters);
 
    // restarts the game
    const retry = () => {
